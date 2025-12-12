904e5a675b8762834255b47f14e6be7a4baab269
dayuan <373556662@qq.com>
2025-12-12 13:57:29 +0900

    film in encoder

--- src/lerobot/policies/fact/modeling_fact.py
+++ src/lerobot/policies/fact/modeling_fact.py
@@ -310,10 +310,10 @@ class FACT(nn.Module):
                 self.vae_encoder_robot_wrench_input_proj = nn.Linear(
                     self.config.wrench_dim, config.dim_model
                 )
-            if self.config.phase_num is not None:
-                self.vae_encoder_phase_input_proj = nn.Embedding(
-                    self.config.phase_num, config.dim_model
-                )
+            # if self.config.phase_num is not None:
+            #     self.vae_encoder_phase_input_proj = nn.Embedding(
+            #         self.config.phase_num, config.dim_model
+                # )
             # Projection layer for action (joint-space target) to hidden dimension.
             self.vae_encoder_action_input_proj = nn.Linear(
                 self.config.selected_action_shape[0],
@@ -328,8 +328,8 @@ class FACT(nn.Module):
                 num_input_token_encoder += 1
             if self.config.wrench_dim is not None:
                 num_input_token_encoder += 1 # modified to account for wrench input
-            if self.config.phase_num is not None:
-                num_input_token_encoder += 1 # modified to account for phase input
+            # if self.config.phase_num is not None:
+            #     num_input_token_encoder += 1 # modified to account for phase input
             self.register_buffer(
                 "vae_encoder_pos_enc",
                 create_sinusoidal_pos_embedding(num_input_token_encoder, config.dim_model).unsqueeze(0),
@@ -384,8 +384,8 @@ class FACT(nn.Module):
             self.n_1d_tokens += 1
         if self.config.wrench_dim is not None:
             self.n_1d_tokens += 1 # modified to account for wrench input
-        if self.config.phase_num is not None:
-            self.n_1d_tokens += 1 # modified to account for phase input
+        # if self.config.phase_num is not None:
+        #     self.n_1d_tokens += 1 # modified to account for phase input
         
         self.encoder_1d_feature_pos_embed = nn.Embedding(self.n_1d_tokens, config.dim_model)
         if self.config.image_features:
@@ -460,9 +460,9 @@ class FACT(nn.Module):
             if self.config.wrench_dim is not None:
                 robot_wrench_embed = self.vae_encoder_robot_wrench_input_proj(batch_wrench)  # (B, D)
                 robot_wrench_embed = robot_wrench_embed.unsqueeze(1)  # (B, 1, D)
-            if self.config.phase_num is not None:
-                phase_embed = self.vae_encoder_phase_input_proj(batch_phase)  # (B, D)
-                phase_embed = phase_embed.unsqueeze(1)  # (B, 1, D)
+            # if self.config.phase_num is not None:
+            #     phase_embed = self.vae_encoder_phase_input_proj(batch_phase)  # (B, D)
+            #     phase_embed = phase_embed.unsqueeze(1)  # (B, 1, D)
 
             selected_action_indices = [0,1,2, 7,8,9,10, 15]
             batch_action = batch[ACTION][..., selected_action_indices]
@@ -470,12 +470,12 @@ class FACT(nn.Module):
             batch_action = torch.cat([batch_action, batch_6d_rot], dim=-1)
             action_embed = self.vae_encoder_action_input_proj(batch_action)  # (B, S, D)
 
-            if self.config.robot_state_feature and self.config.wrench_dim is not None and self.config.phase_num is not None:
-                vae_encoder_input = [cls_embed, robot_state_embed, robot_wrench_embed, phase_embed, action_embed]  # (B, S+4, D)
-            elif self.config.robot_state_feature and self.config.wrench_dim is not None:
+            # if self.config.robot_state_feature and self.config.wrench_dim is not None and self.config.phase_num is not None:
+            #     vae_encoder_input = [cls_embed, robot_state_embed, robot_wrench_embed, phase_embed, action_embed]  # (B, S+4, D)
+            if self.config.robot_state_feature and self.config.wrench_dim is not None:
                 vae_encoder_input = [cls_embed, robot_state_embed, robot_wrench_embed, action_embed]  # (B, S+3, D)
-            elif self.config.robot_state_feature and self.config.phase_num is not None:
-                vae_encoder_input = [cls_embed, robot_state_embed, phase_embed, action_embed]  # (B, S+3, D)
+            # elif self.config.robot_state_feature and self.config.phase_num is not None:
+            #     vae_encoder_input = [cls_embed, robot_state_embed, phase_embed, action_embed]  # (B, S+3, D)
             elif self.config.robot_state_feature:
                 vae_encoder_input = [cls_embed, robot_state_embed, action_embed] # (B, S+2, D)
             else:
@@ -528,8 +528,8 @@ class FACT(nn.Module):
         if self.config.wrench_dim is not None:
             encoder_in_tokens.append(self.encoder_robot_wrench_input_proj(batch_wrench))
         if self.config.phase_num is not None:
-            encoder_phase_embed = self.encoder_phase_input_proj(batch_phase)
-            encoder_in_tokens.append(encoder_phase_embed)
+            encoder_phase_embed = self.encoder_phase_input_proj(batch_phase) # (B, D) as FiLM condition input to encoder
+        #     encoder_in_tokens.append(encoder_phase_embed)
 
         # Environment state token.
         if self.config.env_state_feature:
@@ -558,7 +558,9 @@ class FACT(nn.Module):
         encoder_in_pos_embed = torch.stack(encoder_in_pos_embed, axis=0)
 
         # Forward pass through the transformer modules.
-        encoder_out = self.encoder(encoder_in_tokens, pos_embed=encoder_in_pos_embed)
+        encoder_out = self.encoder(encoder_in_tokens,
+                                    cond= encoder_phase_embed if self.config.phase_num is not None else None,
+                                    pos_embed=encoder_in_pos_embed)
         # TODO(rcadene, alexander-soare): remove call to `device` ; precompute and use buffer
         decoder_in = torch.zeros(
             (self.config.chunk_size, batch_size, self.config.dim_model),
@@ -568,7 +570,6 @@ class FACT(nn.Module):
         decoder_out = self.decoder(
             decoder_in,
             encoder_out,
-            cond= encoder_phase_embed if self.config.phase_num is not None else None,
             encoder_pos_embed=encoder_in_pos_embed,
             decoder_pos_embed=self.decoder_pos_embed.weight.unsqueeze(1),
         )
@@ -592,10 +593,10 @@ class FACTEncoder(nn.Module):
         self.norm = nn.LayerNorm(config.dim_model) if config.pre_norm else nn.Identity()
 
     def forward(
-        self, x: Tensor, pos_embed: Tensor | None = None, key_padding_mask: Tensor | None = None
+        self, x: Tensor, cond: Tensor| None=None, pos_embed: Tensor | None = None, key_padding_mask: Tensor | None = None
     ) -> Tensor:
         for layer in self.layers:
-            x = layer(x, pos_embed=pos_embed, key_padding_mask=key_padding_mask)
+            x = layer(x,cond=cond, pos_embed=pos_embed, key_padding_mask=key_padding_mask)
         x = self.norm(x)
         return x
 
@@ -615,13 +616,19 @@ class ACTEncoderLayer(nn.Module):
         self.dropout1 = nn.Dropout(config.dropout)
         self.dropout2 = nn.Dropout(config.dropout)
 
+        # Conditioning layer (for phase information)
+        self.mlp_mod = _AdaLNMod(config.dim_model)
+        self.mlp_gate = _ZeroGate(config.dim_model)
+        self.mlp_mod.reset_parameters()
+        self.mlp_gate.reset_parameters()
+
         self.activation = get_activation_fn(config.feedforward_activation)
         self.pre_norm = config.pre_norm
 
         if not self.training:
             self.last_attention_weights = None
 
-    def forward(self, x, pos_embed: Tensor | None = None, key_padding_mask: Tensor | None = None) -> Tensor:
+    def forward(self, x, cond: Tensor| None=None, pos_embed: Tensor | None = None, key_padding_mask: Tensor | None = None) -> Tensor:
         skip = x
         if self.pre_norm:
             x = self.norm1(x)
@@ -636,8 +643,15 @@ class ACTEncoderLayer(nn.Module):
         else:
             x = self.norm1(x)
             skip = x
+        # Conditioning with phase information
+        if cond is not None:
+            x = self.mlp_mod(x, cond)
         x = self.linear2(self.dropout(self.activation(self.linear1(x))))
-        x = skip + self.dropout2(x)
+        if cond is not None:
+            x = self.mlp_gate(self.dropout2(x), cond)
+        else:
+            x = self.dropout2(x)
+        x = skip + x
         if not self.pre_norm:
             x = self.norm2(x)
         return x
@@ -678,38 +692,6 @@ class _ZeroGate(nn.Module):
         nn.init.zeros_(self.linear.weight)
         nn.init.zeros_(self.linear.bias)
 
-class _ShiftScaleMod(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.act = nn.SiLU()
-        self.scale = nn.Linear(dim, dim)
-        self.shift = nn.Linear(dim, dim)
-
-    def forward(self, x, c):
-        c = self.act(c)
-        return x * self.scale(c)[None] + self.shift(c)[None]
-
-    def reset_parameters(self):
-        nn.init.xavier_uniform_(self.scale.weight)
-        nn.init.xavier_uniform_(self.shift.weight)
-        nn.init.zeros_(self.scale.bias)
-        nn.init.zeros_(self.shift.bias)
-
-
-class _ZeroScaleMod(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.act = nn.SiLU()
-        self.scale = nn.Linear(dim, dim)
-
-    def forward(self, x, c):
-        c = self.act(c)
-        return x * self.scale(c)[None]
-
-    def reset_parameters(self):
-        nn.init.zeros_(self.scale.weight)
-        nn.init.zeros_(self.scale.bias)
-
 class FACTDecoder(nn.Module):
     def __init__(self, config: FACTConfig):
         """Convenience module for running multiple decoder layers followed by normalization."""
@@ -721,13 +703,12 @@ class FACTDecoder(nn.Module):
         self,
         x: Tensor,
         encoder_out: Tensor,
-        cond: Tensor | None = None, # as Phase information
         decoder_pos_embed: Tensor | None = None,
         encoder_pos_embed: Tensor | None = None,
     ) -> Tensor:
         for layer in self.layers:
             x = layer(
-                x, encoder_out, cond=cond, decoder_pos_embed=decoder_pos_embed, encoder_pos_embed=encoder_pos_embed
+                x, encoder_out, decoder_pos_embed=decoder_pos_embed, encoder_pos_embed=encoder_pos_embed
             )
         if self.norm is not None:
             x = self.norm(x)
@@ -752,16 +733,6 @@ class ACTDecoderLayer(nn.Module):
         self.dropout2 = nn.Dropout(config.dropout)
         self.dropout3 = nn.Dropout(config.dropout)
 
-        # Conditioning layer (for phase information)
-        # self.atten_mod1 = _ShiftScaleMod(config.dim_model)
-        # self.atten_mod2 = _ZeroScaleMod(config.dim_model)
-        # self.mlp_mod1 = _ShiftScaleMod(config.dim_model)
-        # self.mlp_mod2 = _ZeroScaleMod(config.dim_model)
-        self.atten_mod = _AdaLNMod(config.dim_model)
-        self.atten_gate = _ZeroGate(config.dim_model)
-        self.atten_mod.reset_parameters()
-        self.atten_gate.reset_parameters()
-
         self.activation = get_activation_fn(config.feedforward_activation)
         self.pre_norm = config.pre_norm
 
@@ -772,7 +743,6 @@ class ACTDecoderLayer(nn.Module):
         self,
         x: Tensor,
         encoder_out: Tensor,
-        cond: Tensor | None = None, # as Phase information
         decoder_pos_embed: Tensor | None = None,
         encoder_pos_embed: Tensor | None = None,
     ) -> Tensor:
@@ -799,18 +769,13 @@ class ACTDecoderLayer(nn.Module):
             x = self.norm1(x)
             skip = x
         
-        # Conditioning with phase information
-        if cond is not None:
-            x = self.atten_mod(x, cond)
         x = self.multihead_attn(
             query=self.maybe_add_pos_embed(x, decoder_pos_embed),
             key=self.maybe_add_pos_embed(encoder_out, encoder_pos_embed),
             value=encoder_out,
         )[0]  # select just the output, not the attention weights
-        if cond is not None:
-            x = self.atten_gate(self.dropout2(x), cond)
-        else:
-            x = self.dropout2(x)
+        
+        x = self.dropout2(x)
         x = skip + x
 
         if self.pre_norm:
@@ -820,15 +785,8 @@ class ACTDecoderLayer(nn.Module):
             x = self.norm2(x)
             skip = x
 
-        # Conditioning with phase information
-        # if cond is not None:
-        #     x = self.mlp_mod1(x, cond)
         x = self.linear2(self.dropout(self.activation(self.linear1(x))))
-        # if cond is not None:
-        #     x = self.mlp_mod2(self.dropout3(x), cond)
-        # else:
-        x = self.dropout3(x)
-        x = skip + x
+        x = skip + self.dropout3(x)
         if not self.pre_norm:
             x = self.norm3(x)
         return x
